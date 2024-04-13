4b85a46ec1c7730affd8cc4777d2afb2b4ea5e2a
BicycleKix <rydonmorrell@gmail.com>
2024-04-13 14:34:08 -0400

    git config --global user.email "rydonmorrell@gmail.com"   git config --global user.name "BicycleKix"

---
+++ game.py
@@ -0,0 +1,117 @@
+import sys
+import random
+import math
+import os
+
+import pygame
+
+from scripts.utils import load_image, load_images, load_player, Animation
+from scripts.tilemap import Tilemap
+from scripts.entities import Player
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Ninja Fighter')
+        self.screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+        self.character_count = len(os.listdir('data/images/player_selection'))
+        self.characters = sorted(os.listdir('data/images/player_selection'))
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'background': pygame.transform.scale(load_image('background.png'),self.display.get_size()),
+            'clouds': load_images('clouds'),
+            'players': load_images('player_selection', alt=True),
+        }
+
+        self.player_size = (8, 15)
+
+        self.movement1 = [False, False, False]
+        self.movement2 = [False, False, True]
+
+        self.tilemap = Tilemap(self)
+
+        self.player = [Player(self, (50, 50), (8, 15)), Player(self, (50, 50), (8, 15))]
+
+        self.load_level('map')
+
+    def load_level(self, map_id):
+        self.tilemap.load('data/maps/' + str(map_id) + '.json')
+
+        spawn_points = self.tilemap.extract([('spawners', 0)]).copy()
+
+        for player in self.player:
+            player.pos = random.choice(spawn_points)
+            player.air_time = 0
+            spawn_points.remove(player.pos)           
+
+    def start(self):
+
+        choice = [0, 0]
+        confirm = [False, False]
+
+        while True:
+            self.display.blit(self.assets['background'], (0,0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN and all(confirm):
+                        self.run()
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_a:
+                        choice[0] = (choice[0] + 1) % self.character_count
+                    if event.key == pygame.K_d:
+                        choice[0] = (choice[0] - 1) % self.character_count
+                    if event.key == pygame.K_w:
+                        confirm[0] = not confirm[0]
+                    if event.key == pygame.K_LEFT:
+                        choice[1] = (choice[1] + 1) % self.character_count
+                    if event.key == pygame.K_RIGHT:
+                        choice[1] = (choice[1] - 1) % self.character_count
+                    if event.key == pygame.K_UP:
+                        confirm[1] = not confirm[1]
+
+            self.display.blit(pygame.transform.scale(self.assets['players'][choice[0]], (160, 160)), (0, 40))
+            self.display.blit(pygame.transform.flip(pygame.transform.scale(self.assets['players'][choice[1]], (160, 160)), True, False), (150, 40))
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+    def run(self):
+        run = True
+
+        while run:
+            self.display.blit(self.assets['background'], (0,0))
+
+            self.tilemap.render(self.display)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().start()
---
+++ scripts/utils.py
@@ -0,0 +1,51 @@
+import os
+
+import pygame
+
+BASE_IMG_PATH = 'data/images/'
+
+def load_image(path):
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img.set_colorkey((0,0,0))
+    return img
+
+def load_png(path):
+    img = pygame.image.load(BASE_IMG_PATH + path).convert_alpha()
+    return img
+
+def load_images(path, alt=False):
+    images = []
+    if alt:
+        for img_name in os.listdir(BASE_IMG_PATH + path):
+            images.append(load_png(path + '/' + img_name))
+    else:
+        for img_name in os.listdir(BASE_IMG_PATH + path):
+            images.append(load_image(path + '/' + img_name))
+    return images
+
+def load_player(player):
+    sprite = []
+    for folder in os.listdir(BASE_IMG_PATH + player):
+        sprite.append(load_images(player + '/' + folder))
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True):
+        self.images = images
+        self.loop = loop
+        self.img_duration = img_dur
+        self.done = False
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.img_duration, self.loop)
+    
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
+    
+    def img(self):
+        return self.images[int(self.frame / self.img_duration)]
